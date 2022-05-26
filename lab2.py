8725cd23fb6b0263ce63aa1dc7588a7eaf37cc82
mahlio2002 <106351952+mahlio2002@users.noreply.github.com>
2022-05-27 02:48:49 +0400

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -6,16 +6,16 @@ import re
 from collections import Counter
 try:
     d = Counter()
-    filePath = input('Введите путь к файлу: ')
+    filePath = input('Путь к файлу: ')
     with open(filePath, 'r', encoding='utf8') as file:
         for line in file:
           d.update(line.strip(',.\n'))
         file.close()
         print(d)
-        print("Время выполнения: " + str(time.process_time()))
+        print("Время: " + str(time.process_time()))
 except FileNotFoundError:
     print('Файл *.txt не обнаружен.')
-    print("Время выполнения: " + str(time.process_time()))
+    print("Время: " + str(time.process_time()))
 except ValueError:
-    print('Вы ввели некорректное значение.')
-    print("Время выполнения: " + str(time.process_time()))
+    print('Некорректное значение.')
+    print("Время: " + str(time.process_time()))
